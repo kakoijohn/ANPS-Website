8f44eae2f8460b464f3b6ba92fb7781c79ba3da7
John <kakoijohn@gmail.com>
2018-11-16 13:59:28 -0500

    construction sign

--- scripts/site_script.js
+++ scripts/site_script.js
@@ -13,6 +13,10 @@ $(document).ready(function() {
 		$('.full_context_menu').toggleClass('full_context_menu--active', true);
 	});
 
+	$('#drop_only_menu').click(function() {
+		$('.full_context_menu').toggleClass('full_context_menu--active', true);
+	});	
+
 	$('#drop_down_menu-exit').click(function() {
 		$('.full_context_menu').toggleClass('full_context_menu--active', false);
 	});
